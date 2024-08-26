b68cf8fb7dea44cc1146b26c10e04cf4283aa7d6
ArmsTina <didehddud1005@gmail.com>
2024-08-25 22:04:25 -0700

    useState updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Practice_useState from './hooks/Practice_useState';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ol className="lists">
+        <div>
+          <li>useState</li>
+          <Practice_useState/>
+        </div>
+        <div>
+          <li>useEffect</li>
+        </div>
+      </ol>
+    </>
   );
 }
 
---
+++ src/hooks/Practice_useState.js
@@ -0,0 +1,52 @@
+import {useState} from 'react';
+
+export default function Practice_useState(){
+    // 1.useState
+    // when there is an update, it will re-render the component
+    const [time, setTime] = useState(5);
+
+    const timeIncrease = () => {
+        // When trying to change the state, it is better to set a new value
+        let newTime = time;
+        newTime === 23 ? setTime(0) : setTime(newTime+1);
+    }
+
+    // If you want to get an initial value from outside
+    const testWork = () => {
+        console.log('This is a heavy work');
+        return ['Kim', 'Yang'];
+    }
+    // Don't call right away - It will call testWork every render
+    // Instead, use Callback function. () => { return functionName(); }
+    const [names, setNames] = useState(()=>{ return testWork(); });
+    const [input, setInput] = useState('');
+
+    const saveInput = (e) => {
+        setInput(e.target.value);
+    }
+
+    const saveName = () => {
+        // The callback receives the current state (prevState) and returns the new state to be set by setNames
+        setNames((prevState) => {
+            return [...prevState, input];
+        });
+        setInput('');
+    }
+
+    return(
+        <>
+            <span>Current Time: {time}:00 </span>
+            <button onClick={timeIncrease}>Update Time</button>
+            <p></p>
+            <input type="text" value={input} onChange={saveInput}/>
+            <button onClick={saveName}>Upload</button>
+
+            {/* Iterate while names has value */}
+            {/* arr.map(function(element, index, whole_array){  }); */}
+            {/* A key is necessary when using arr.map() */}
+            {names.map((name, index) => {
+                return <p key={index}>{name}</p>
+            })}
+        </>
+    )
+}
