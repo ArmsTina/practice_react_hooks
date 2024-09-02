af4b5a29a48efb12c28ca6cbf963ede97dccc076
ArmsTina <didehddud1005@gmail.com>
2024-09-01 19:06:44 -0700

    useRef1 Updated

--- src/App.js
+++ src/App.js
@@ -4,24 +4,26 @@ import Practice_useState from './hooks/Practice_useState';
 import {Routes, Route, Link} from 'react-router-dom';
 import {HStack} from '@chakra-ui/react'
 import Practice_useEffect2 from './hooks/Practice_useEffect2';
+import Practice_useRef2 from './hooks/Practice_useRef2';
+import Practice_useRef1 from './hooks/Practice_useRef1';
 
+// 새로고침이 페이지 전체를 렌더링 하지만, 리액트를 사용시 변경된 부분만 렌더링 한다.
 function App() {
   return (
     <>
       <HStack>
-        <div>
-          <nav><Link to="/usestate">useState</Link></nav>
-        </div>
-        <div>
-          <nav><Link to="/useeffect1">useEffect 1</Link></nav>
-        </div>
-        <div>
-          <nav><Link to="/useeffect2">useEffect 2</Link></nav>
-        </div>
+        <nav><Link to="/usestate">useState</Link></nav>
+        <nav><Link to="/useeffect1">useEffect 1</Link></nav>
+        <nav><Link to="/useeffect2">useEffect 2</Link></nav>
+        <nav><Link to="/useeffect2">useEffect 2</Link></nav>
+        <nav><Link to="/useref1">useRef 1</Link></nav>
+        <nav><Link to="/useref2">useRef 2</Link></nav>
       </HStack>
       <Routes><Route path="/usestate" element={<Practice_useState/>}/></Routes>
       <Routes><Route path="/useeffect1" element={<Practice_useEffect1/>}/></Routes>
       <Routes><Route path="/useeffect2" element={<Practice_useEffect2/>}/></Routes>
+      <Routes><Route path="/useref1" element={<Practice_useRef1/>}/></Routes>
+      <Routes><Route path="/useref2" element={<Practice_useRef2/>}/></Routes>
     </>
   );
 }
