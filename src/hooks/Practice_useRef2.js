dd0989aa8b765f333ba56b35fc4435b6c7638503
ArmsTina <didehddud1005@gmail.com>
2024-09-02 00:06:16 -0700

    Update useRef2

--- src/hooks/Practice_useRef2.js
+++ src/hooks/Practice_useRef2.js
@@ -3,11 +3,16 @@ import { useEffect, useRef } from 'react';
 export default function Practice_useRef2() {
     // useRef can also be used to easily access DOM elements.
     const inputRef = useRef();
+    const buttonRef = useRef();
 
     // Using ${variable} with backticks (`) to easily concatenate strings and variables.
     const handleFocus = () => {
         alert(`Welcome, ${inputRef.current.value}`);
         inputRef.current.focus();
+        // You can modify attributes
+        inputRef.current.value = '';
+        inputRef.current.placeholder = 'Refresh the page.';
+        buttonRef.current.disabled = true;
     }
 
     // Automatically focuses on the input field when the component first renders.
@@ -22,7 +27,7 @@ export default function Practice_useRef2() {
                 ref={inputRef} 
                 placeholder="username" 
             /> {/* inputRef now points to this input element */}
-            <button onClick={handleFocus}>Login</button>
+            <button onClick={handleFocus} ref={buttonRef}>Login</button>
         </>
     );
 }
